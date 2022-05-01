781575eae9e7fc72ca5052d882b63c99746561a7
Henrique Nunes <hrn2001@gmail.com>
2022-05-01 22:36:38 +0100

    Clean up main

--- assign2/src/Main.java
+++ assign2/src/Main.java
@@ -1,23 +1,7 @@
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        String test = "abscd";
-        MessageDigest digest = null;
-        try {
-            digest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        byte[] hash1 = digest.digest(test.getBytes(StandardCharsets.UTF_8));
-
-        String keyHased = String.format("%064x", new BigInteger(1, digest.digest(test.getBytes(StandardCharsets.UTF_8))));
-        System.out.println(keyHased);
 
     }
 }
