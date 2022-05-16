997f5b07b308bc9f8a42c26eae1d1eac0476a8ae
Patricia Oliveira <up201905427@fe.up.pt>
2022-05-16 19:07:23 +0100

    HashUtils (getHashedSha256)

---
+++ assign2/src/utils/HashUtils.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtils {
+
+    public static String getHashedSha256(String value) {
+        try {
+            MessageDigest encoder = MessageDigest.getInstance("SHA-256");
+
+            return String.format("%064x", new BigInteger(1,
+                encoder.digest(value.getBytes(StandardCharsets.UTF_8))));
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("SHA-256 doesn't exist!");
+            return null;
+        }
+    }
+}
